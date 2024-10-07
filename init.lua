8830e3809d3ee539efa227af16b6ff8029d7eb0c
Evan Kuo <evan.kuo.edu@gmail.com>
2024-10-07 02:35:45 -0500

    added neotree

--- init.lua
+++ init.lua
@@ -906,7 +906,7 @@ require('lazy').setup({
   require 'kickstart.plugins.indent_line', -- adds vertical lines to show indent levels
   -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
