dc57a6f49e38fb945ce25abd030486dae903530c
Evan Kuo <evan.kuo.edu@gmail.com>
2024-11-11 22:15:53 -0600

    changes

--- init.lua
+++ init.lua
@@ -673,7 +673,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, python = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
@@ -937,7 +937,7 @@ require('lazy').setup({
   require 'plugins.treesj',
   require 'plugins.lazygit',
   require 'plugins.yazi',
-  require 'plugins.neoscroll',
+  -- require 'plugins.neoscroll',
   require 'plugins.harpoon',
 
   require 'plugins.outline',
