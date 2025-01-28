163e31f65a63375ba9f2fde9cbba5b210bb3fec8
Evan Kuo <evan.kuo.edu@gmail.com>
2025-01-28 00:07:05 -0600

    eyeliner

--- init.lua
+++ init.lua
@@ -959,6 +959,7 @@ require('lazy').setup({
   require 'plugins.treesitter_context',
 
   require 'plugins.vimtex',
+  require 'plugins.eyeliner',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
