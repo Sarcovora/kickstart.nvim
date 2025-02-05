47f84bc3d1d9ada9b8ea008aadf9c9cc64e0f448
Evan Kuo <evan.kuo.edu@gmail.com>
2025-02-05 10:26:45 -0600

    todo comments

--- init.lua
+++ init.lua
@@ -846,7 +846,27 @@ require('lazy').setup({
     'folke/todo-comments.nvim',
     event = 'VimEnter',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
+    opts = {
+      signs = false,
+      keywords = {
+        -- TODO:
+        -- BUG:
+        -- HACK:
+        -- WARN:
+        -- PERF:
+        -- NOTE:
+        -- TEST:
+        -- HELP:
+        FIX = { icon = ' ', color = 'error', alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' } },
+        TODO = { icon = ' ', color = 'info' },
+        HACK = { icon = ' ', color = 'warning' },
+        WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+        PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+        NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+        TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+        HELP = { icon = ' ', color = '#ebbcba', alt = { 'ASK' } },
+      },
+    },
   },
 
   { -- Collection of various small independent plugins/modules
