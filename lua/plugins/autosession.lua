8abaead834370829bf4c2469b280ac5458bb54e1
Evan Kuo <evan.kuo.edu@gmail.com>
2025-04-02 16:54:17 -0500

    auto-session setup

--- lua/plugins/autosession.lua
+++ lua/plugins/autosession.lua
@@ -6,6 +6,7 @@ return {
   ---@module "auto-session"
   ---@type AutoSession.Config
   opts = {
+    enabled = os.getenv 'NO_AUTO_SESSION' ~= '1', -- disable if NO_AUTO_SESSION env var is set
     suppressed_dirs = { '~/', '~/Downloads', '/' },
     -- log_level = 'debug',
   },
