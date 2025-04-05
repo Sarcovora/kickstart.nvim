d907f48cd27a22c37a937d2d12f1140dc71c6274
Evan Kuo <evan.kuo.edu@gmail.com>
2025-04-04 22:17:46 -0500

    treesj

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -3,6 +3,8 @@ return {
   keys = { '<leader>m' },
   dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
   config = function()
-    require('treesj').setup {}
+    require('treesj').setup {
+      max_join_length = 240,
+    }
   end,
 }
