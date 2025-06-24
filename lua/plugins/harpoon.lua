1464e005b5fa645e681c012f489f4cdada9a579a
Evan Kuo <evan.kuo.edu@gmail.com>
2025-06-23 23:39:46 -0500

    harpoon dynamic list

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,7 +10,12 @@ return {
     harpoon:setup {}
 
     vim.keymap.set('n', '<leader>hl', function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
+      local list = harpoon:list()
+      local height = math.max(8, list:length())
+      harpoon.ui:toggle_quick_menu(list, {
+        height_in_lines = height,
+        border = 'rounded',
+      })
     end, { desc = '[H]arpoon: List files with Telescope' })
 
     vim.keymap.set('n', '<leader>ha', function()
@@ -49,64 +54,3 @@ return {
     end, { desc = '[H]arpoon: select buffer (10)' })
   end,
 }
-
--- NOTE: This was for a custom picker that I was trying to make, but it's not actually much better at all than the normal harpoon picker
-
--- -- Define a custom highlight group for the numbers (muted purple)
--- vim.api.nvim_set_hl(0, 'HarpoonNumber', { fg = '#9d7cd8', bold = true }) -- Customize this to your preferred color
--- vim.keymap.set('n', '<leader>hl', function()
---   local harpoon_files = harpoon:list()
---   local conf = require('telescope.config').values
---   local entry_display = require 'telescope.pickers.entry_display'
---
---   -- Define the format for how the entries should be displayed
---   local displayer = entry_display.create {
---     separator = ' ',
---     items = {
---       { width = 5, hl_group = 'HarpoonNumber' }, -- Number with custom highlight
---       { remaining = true }, -- File path
---     },
---   }
---
---   -- Create a list of items that contains both the index and file path
---   local file_items = {}
---   for idx, item in ipairs(harpoon_files.items) do
---     table.insert(file_items, { idx = idx, path = item.value })
---   end
---
---   -- Telescope picker setup
---   require('telescope.pickers')
---     .new({}, {
---       prompt_title = 'Harpoon',
---       finder = require('telescope.finders').new_table {
---         results = file_items,
---         entry_maker = function(entry)
---           return {
---             value = entry.path,
---             display = function(display_entry)
---               return displayer {
---                 { tostring(display_entry.idx), 'HarpoonNumber' }, -- Number with color
---                 display_entry.path, -- File path
---               }
---             end,
---             ordinal = entry.path,
---             idx = entry.idx,
---             path = entry.path,
---           }
---         end,
---       },
---       previewer = conf.file_previewer {},
---       sorter = conf.generic_sorter {},
---       attach_mappings = function(_, map)
---         -- Define what happens when <CR> (enter) is pressed
---         map('i', '<CR>', function(prompt_bufnr)
---           local selection = require('telescope.actions.state').get_selected_entry()
---           require('telescope.actions').close(prompt_bufnr)
---           -- Open the selected file
---           vim.cmd('edit ' .. selection.value)
---         end)
---         return true
---       end,
---     })
---     :find()
--- end, { desc = '[H]arpoon: List files with Telescope' })
