2eaa6ae285d9b5b0f6739b0fdcafaa8d33634d08
ekuo-foreflight <ekuo@foreflight.com>
2025-06-24 14:36:25 -0500

    buffer manager more full config

--- lua/plugins/buffer-manager.lua
+++ lua/plugins/buffer-manager.lua
@@ -1,10 +1,28 @@
 return {
   'j-morano/buffer_manager.nvim',
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    require("buffer_manager").setup({})
+    require('buffer_manager').setup {
+      width = 0.9,
+      select_menu_item_commands = {
+        v = {
+          key = '<C-v>',
+          command = 'vsplit',
+        },
+        h = {
+          key = '<C-x>',
+          command = 'split',
+        },
+      },
+    }
   end,
   keys = {
-    { "<leader>bl", function() require("buffer_manager.ui").toggle_quick_menu() end, desc = "Toggle buffer manager" },
+    {
+      '<leader>bl',
+      function()
+        require('buffer_manager.ui').toggle_quick_menu()
+      end,
+      desc = 'Toggle buffer manager',
+    },
   },
 }
