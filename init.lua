b1a60b79adb127385be474d19f9c0ae0d8a24a2b
Evan Kuo <evan.kuo.edu@gmail.com>
2025-06-24 02:17:58 -0500

    more cleanup

--- init.lua
+++ init.lua
@@ -225,30 +225,7 @@ vim.env.MASON_PYTHON = '/usr/bin/python3'
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-  --
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
-  --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `config` key, the configuration only runs
-  -- after the plugin has been loaded:
-  --  config = function() ... end
-
-  -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  { 'NMAC427/guess-indent.nvim', opts = { auto_cmd = true } }, -- Detect tabstop and shiftwidth automatically
-
+  require 'plugins.guess-indent', -- Automatically detect tabstop and shiftwidth
   require 'plugins.whichkey', -- useful plugin to show you pending keybinds.
   require 'plugins.telescope', -- fuzzy finder, file search, etc
 
---
+++ lua/plugins/guess-indent.lua
@@ -0,0 +1,9 @@
+return {
+  'NMAC427/guess-indent.nvim',
+  opts = {
+    auto_cmd = true,
+    on_tab_options = {
+      ['expandtab'] = true,
+    },
+  },
+}
