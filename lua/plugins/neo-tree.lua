d0a8f3d2ad050f1fe1335355291a40bb8db8a2ef
ekuo-foreflight <ekuo@foreflight.com>
2025-07-07 11:48:12 -0500

    neotree keybindings and behavioral change

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,28 +8,61 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    'folke/snacks.nvim',
   },
   lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    close_if_last_window = true,
+    popup_border_style = 'NC',
+    enable_git_status = true,
+    enable_diagnostics = true,
     filesystem = {
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['z'] = false,
+          ['zc'] = 'close_all_nodes',
         },
       },
       filtered_items = {
         visible = true,
         hide_dotfiles = false,
-        hide_gitignored = true,
+        hide_gitignored = false,
         hide_by_name = {
           '.git',
           '.github',
           '.DS_Store',
           'thumbs.db',
         },
+      },
+    },
+    default_component_configs = {
+      -- If you don't want to use these columns, you can set `enabled = false` for each of them individually
+      file_size = {
+        enabled = false,
+        width = 12, -- width of the column
+        required_width = 64, -- min width of window required to show this column
+      },
+      type = {
+        enabled = false,
+        width = 10, -- width of the column
+        required_width = 122, -- min width of window required to show this column
+      },
+      last_modified = {
+        enabled = true,
+        width = 20, -- width of the column
+        required_width = 110, -- min width of window required to show this column
+      },
+      created = {
+        enabled = false,
+        width = 20, -- width of the column
+        required_width = 110, -- min width of window required to show this column
+      },
+      symlink_target = {
+        enabled = false,
       },
     },
   },
